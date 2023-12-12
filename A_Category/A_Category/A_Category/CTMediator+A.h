ffdf83ba2d0b6daac48b06deaf488ae5be18832e
zhourui <zhourui@agora.io>
2023-12-13 01:40:57 +0800

    publish 1.0.0

---
+++ A_Category/A_Category/A_Category/CTMediator+A.h
@@ -0,0 +1,18 @@
+//
+//  CTMediator+A.h
+//  A_Category
+//
+//  Created by ZhouRui on 2023/12/13.
+//
+
+#import <CTMediator/CTMediator.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (A)
+
+- (UIViewController *)A_aViewController;
+
+@end
+
+NS_ASSUME_NONNULL_END
